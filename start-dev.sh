36caab12f0782ce5bbe1ff1140878ceb4ef6667d
peterh <peter.horvath@baranidesign.com>
2025-04-30 21:46:44 +0200

    Upgrade Docker Compose version and enhance start-dev script for improved service management

--- start-dev.sh
+++ start-dev.sh
@@ -1,7 +1,13 @@
+docker compose down
+echo "Building and starting the services..."
 docker compose up -d --build
 echo "Waiting for the services to start..."
 sleep 10
+echo "Running clear cache..."
+docker compose exec -it app php artisan config:clear
+docker compose exec -it app php artisan cache:clear
+docker compose exec -it app php artisan route:clear
+docker compose exec -it app php artisan view:clear
 echo "Running migrations and seeding the database..."
-docker compose exec -it app php artisan migrate
-docker compose exec -it app php artisan db:seed
+docker compose exec -it app php artisan migrate:fresh --seed
 echo "Done!"
