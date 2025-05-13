beb452971ee2efbcb0c94615de8837e6dff6250b
peterh <peter.horvath@baranidesign.com>
2025-05-14 00:29:27 +0200

    Edit nginx config and vite updates

--- vite.config.js
+++ vite.config.js
@@ -8,19 +8,26 @@ const disableHash = process.env.NO_HASH === 'true';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
         vue(),
+        tailwindcss(),
     ],
     build: {
         outDir: 'dist',
         rollupOptions: {
+            manualChunks: undefined,
             output: {
-                entryFileNames: disableHash ? 'assets/[name].js' : 'assets/[name].[hash].js',
-                chunkFileNames: disableHash ? 'assets/[name].js' : 'assets/[name].[hash].js',
-                assetFileNames: disableHash ? 'assets/[name][extname]' : 'assets/[name].[hash][extname]',
+                entryFileNames: disableHash
+                    ? 'assets/app.js'
+                    : 'assets/app.[hash].js',
+                chunkFileNames: disableHash
+                    ? 'assets/[name].js'
+                    : 'assets/[name].[hash].js',
+                assetFileNames: disableHash
+                    ? 'assets/[name][extname]'
+                    : 'assets/[name].[hash][extname]',
             },
         },
     },
