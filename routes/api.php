6cc59a1e1f5b149a3630ec6fa935a6e7b29a1052
peter <ph@mastery.sk>
2025-05-14 22:09:22 +0200

    update docker file and routes

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\CategoryController;
 
+
 Route::prefix('products')->group(function () {
     Route::get('/', [ProductController::class, 'index']);    // GET /api/products
     Route::get('{id}', [ProductController::class, 'show']);   // GET /api/products/{id}
